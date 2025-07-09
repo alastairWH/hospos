1a4a6f7d21ca3d5858bc3e1e254b44abf256838c
alastairwh <alastair@wilsonhouse.uk>
2025-07-09 12:13:09 +0100

    Refactor database initialization to ensure all collections are created and seeded with initial data

--- Backend/internal/dbinit/dbinit.go
+++ Backend/internal/dbinit/dbinit.go
@@ -11,6 +11,22 @@ import (
 )
 
 // SeedData holds initial data for collections
+// List of all collections to ensure they are created
+var AllCollections = []string{
+	"users",
+	"roles",
+	"discounts",
+	"bookings",
+	"locations",
+	"inventory",
+	"products",
+	"categories",
+	"sales",
+	"payments",
+	"receipts",
+	"reminders",
+}
+
 var SeedData = map[string][]interface{}{
 	"users": {
 		bson.M{"name": "admin", "role": "admin"},
@@ -26,6 +42,32 @@ var SeedData = map[string][]interface{}{
 func InitDB() error {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
+
+	// Ensure all collections exist by inserting a dummy doc if empty, then removing it
+	for _, collName := range AllCollections {
+		coll, err := db.GetCollection(collName)
+		if err != nil {
+			log.Printf("dbinit: failed to get collection %s: %v", collName, err)
+			return err
+		}
+		count, err := coll.CountDocuments(ctx, bson.M{})
+		if err != nil {
+			return err
+		}
+		if count == 0 {
+			// Insert a dummy doc to create the collection
+			res, err := coll.InsertOne(ctx, bson.M{"_init": true})
+			if err != nil {
+				log.Printf("dbinit: failed to create %s: %v", collName, err)
+				return err
+			}
+			// Remove the dummy doc
+			_, _ = coll.DeleteOne(ctx, bson.M{"_id": res.InsertedID})
+			log.Printf("dbinit: ensured collection %s exists", collName)
+		}
+	}
+
+	// Seed data for collections that need it
 	for collName, docs := range SeedData {
 		coll, err := db.GetCollection(collName)
 		if err != nil {
